5cd9abbb55e11be061d98219d91b0655de4d6d30
gramz <gramz@LAB4-CAT>
2025-05-12 08:15:03 -0600

    Agregar archivos de proyecto.

---
+++ Models/Starship.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Formularios.Models
+{
+    public class Starship
+    {
+        [Required]
+        [StringLength(5,
+            ErrorMessage = "Identificador muy largo (máximo 5 letras)")]
+        [Display(Name = "Identificador de la Nave")]
+        public string Identifier { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Classification { get; set; }
+
+        [Range(1, 100,
+            ErrorMessage = "Capacidad entre (1-100).")]
+        public int MaximumAccommodation { get; set; }
+
+        [Required]
+        [Range(typeof(bool), "true", "true",
+            ErrorMessage = "This form disallows unapproved ships.")]
+        public bool IsValidatedDesign { get; set; }
+
+        [Required]
+        public DateTime ProductionDate { get; set; }
+    }
+}
